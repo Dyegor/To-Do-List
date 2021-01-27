74b49848f55e5a4b1b33fcaa5fe84e0bdf3169d6
dyegor <dmitriy.yegorov85@gmail.com>
2021-01-28 12:37:55 +1300

    Client side initial setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import BodyContainer from "./components/body-components/BodyContainer";
+
+export default class App extends Component {
+    render() {
+        return (
+          <BodyContainer />
+        );
+    }
+}
