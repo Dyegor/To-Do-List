b225b5a437e61c5e3d39e930965f5265bc4547c2
dyegor <dmitriy.yegorov85@gmail.com>
2021-02-07 18:37:40 +1300

    Add BodyContainer component

--- client/src/components/body-components/BodyContainer.jsx
+++ client/src/components/body-components/BodyContainer.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
-import Footer from "../footer-components/Footer";
+import uuid from 'react-uuid';
+import axios from 'axios';
+import Context from "../context/Context";
 import InputContainer from "./input-container-components/InputContainer";
+import Footer from "../footer-components/Footer";
 
 export default class BodyContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            listName: '',
+            existingListNames: [],
+
+            item: {
+                id: '',
+                toDo: ''
+            },
+            listItems: []
+        };
+    }
+
     render() {
+
         return (
             <div className="main-container">
-                <InputContainer />
+                <Context.Provider>
+                    <InputContainer />
+                </Context.Provider>
                 <Footer />
             </div>
         );
